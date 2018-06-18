8e6b6e3fb64dbb8b13a882ab6bebcaf461bb07f3
zorarara <37677929+zorarara@users.noreply.github.com>
2018-06-18 14:40:46 +0800

    Create main.py

---
+++ hw01/main.py
@@ -0,0 +1,66 @@
+
+<!DOCTYPE html>
+<html>
+
+<body>
+    <div>
+        <table width="580" border="0">
+            <tr>
+                <td colspan="2" style="background-color:#99bbbb;text-align:center">
+                    <h1>Main Title of Web Page</h1>
+                </td>
+            </tr>
+
+            <tr>
+                <td>
+                    <table style="margin:0px;">
+                        <tr>
+                            <td style="background-color:#ffff99;width:200px;text-align:top;">
+                                <b>Menu</b>
+                                <br /> HTML
+                            </td>   
+                            <td style="background-color:#EEEEEE;height:100px;width:200px;text-align:center;">
+                                Content goes here</td>
+                            <td style="background-color:rgba(46, 165, 145, 0.667);height:100px;width:200px;text-align:center;">
+                                Content goes here</td>
+
+                        </tr>
+                        <tr>
+                            <td style="background-color:#ffff99;width:200px;text-align:top;">
+                                <b>Menu</b>
+                                <br /> CSS
+                            </td>
+                            <td style="background-color:#EEEEEE;height:100px;width:200px;text-align:center;">
+                                Content goes here</td>
+                            <td style="background-color:rgba(46, 165, 145, 0.667);height:100px;width:200px;text-align:center;">
+                                Content goes here</td>
+                            <tr>
+                                <td style="background-color:#ffff99;width:200px;text-align:top;">
+                                    <b>Menu</b>
+                                    <br /> JavaScript
+                                </td>
+                                <td style="background-color:#EEEEEE;height:100px;width:200px;text-align:center;">
+                                    Content goes here</td>
+                                <td style="background-color:rgba(46, 165, 145, 0.667);height:100px;width:200px;text-align:center;">
+                                    Content goes here</td>
+
+                            </tr>
+
+
+                        </tr>
+
+                    </table>
+                </td>
+            </tr>
+
+            <tr>
+                <td colspan="2" style="background-color:#99bbbb;text-align:center;">
+                    Copyright W3School.com.cn</td>
+            </tr>
+    
+        </table>
+    </div>
+
+</body>
+
+</html>
